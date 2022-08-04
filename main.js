28b99d377b5a813b50ed9099a62cfc943a7e1fa7
John-Michael <johnmcnagy@gmail.com>
2022-08-04 15:41:04 -0400

    Fixed reset btn and added coloring toggle/status

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let color = "black";
+let click = true;
 
 function populateBoard(size) {
   let board = document.querySelector(".board");
-  let squares = board.querySelectorAll(".div");
+  let squares = board.querySelectorAll("div");
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
@@ -27,10 +28,12 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-  if (color == "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  } else {
-    this.style.backgroundColor = color;
+  if (click) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
   }
 }
 
@@ -40,6 +43,17 @@ function changeColor(choice) {
 
 function resetBoard() {
   let board = document.querySelector(".board");
-  let squares = board.querySelectorAll(".div");
-  squares.forEach((div) => div.remove());
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
+
+document.querySelector("body").addEventListener("click", (e) => {
+  if (e.target.tagName != "BUTTON") {
+    click = !click;
+    if (click) {
+      document.querySelector(".mode").textContent = "Mode: Coloring";
+    } else {
+      document.querySelector(".mode").textContent = "Mode: Not Coloring";
+    }
+  }
+});
